e25c3d8cfea8683c33fbd33cdd932181ec8e325f
jimtin <jameshin84@gmail.com>
2020-07-11 06:54:42 +1000

    Moved around a couple of items

--- runcommand.ps1
+++ runcommand.ps1
@@ -5,8 +5,4 @@ $playbook = $env:PLAYBOOK
 $message = "Running playbook " + $playbook + " against " + $target
 Write-Information -InformationAction Continue -MessageData $message
 
-# Get the current IP address
-$ipaddress = Get-NetIPAddress
-Write-Host $ipaddress
-
 Invoke-Pester "C:\\WindowsForensicsGatherer\\CFF_WindowsForensicsGatherer-master\\Tests\\inittest.Tests.ps1"
