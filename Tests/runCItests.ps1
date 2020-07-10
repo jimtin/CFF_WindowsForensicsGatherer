83e099ac7510f35be82def3cebd9a1eb3a39a604
jimtin <jameshin84@gmail.com>
2020-07-11 07:16:57 +1000

    Hopefully now have a bit of a testing environment set up

--- Tests/runCItests.ps1
+++ Tests/runCItests.ps1
@@ -2,4 +2,4 @@
 $netipaddress = Get-NetIPAddress
 
 # Pass the computername into the container. It does seem a little recursive in nature but is important
-docker run -e Target=$env:COMPUTERNAME -e Playbook="TestPlaybook" -t windowsforensicsgatherer
+docker run -e Target=$env:COMPUTERNAME -e Playbook="CITests" -t windowsforensicsgatherer
