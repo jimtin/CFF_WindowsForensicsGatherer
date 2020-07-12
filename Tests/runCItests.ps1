ad04835e1dbd23688611c36e459c587ca8f04d7c
jimtin <jameshin84@gmail.com>
2020-07-13 05:46:45 +1000

    Working on getting the right IP address passed in

--- Tests/runCItests.ps1
+++ Tests/runCItests.ps1
@@ -1,6 +1,7 @@
 # Get the hostname and IP of this endpoint
 $netipaddress = Get-NetIPAddress | Where-Object {$_.InterfaceAlias -eq "Ethernet"}
 $netipaddress = $netipaddress | Where-Object {$_.AddressFamily -eq "IPv4"}
+Write-Host $netipaddress
 
 # Pass the computername into the container. It does seem a little recursive in nature but is important
 docker run -e Target=$netipaddress -e Playbook="CITests" -t windowsforensicsgatherer
