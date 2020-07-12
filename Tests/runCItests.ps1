3019f6c107b164c91ea1bb43195720cd10316d86
jimtin <jameshin84@gmail.com>
2020-07-13 05:51:16 +1000

    Working on getting the right IP address passed in

--- Tests/runCItests.ps1
+++ Tests/runCItests.ps1
@@ -1,6 +1,8 @@
 # Get the hostname and IP of this endpoint
 $netipaddress = Get-NetIPAddress | Where-Object {$_.InterfaceAlias -eq "Ethernet"}
 $netipaddress = $netipaddress | Where-Object {$_.AddressFamily -eq "IPv4"}
+# Turn it into a string
+$netipaddress = $netipaddress.ToString()
 Write-Host $netipaddress
 
 # Pass the computername into the container. It does seem a little recursive in nature but is important
