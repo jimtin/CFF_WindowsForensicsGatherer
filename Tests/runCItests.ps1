1aac9c3fba99b9aa79dfcd3fbb87a62a356d7bc1
jimtin <jameshin84@gmail.com>
2020-07-13 05:43:17 +1000

    Updated to pull from repository

--- Tests/runCItests.ps1
+++ Tests/runCItests.ps1
@@ -3,4 +3,4 @@ $netipaddress = Get-NetIPAddress | Where-Object {$_.InterfaceAlias -eq "Ethernet
 $netipaddress = $netipaddress | Where-Object {$_.AddressFamily -eq "IPv4"}
 
 # Pass the computername into the container. It does seem a little recursive in nature but is important
-docker run -e Target=$env:COMPUTERNAME -e Playbook="CITests" -t windowsforensicsgatherer
+docker run -e Target=$netipaddress -e Playbook="CITests" -t windowsforensicsgatherer
