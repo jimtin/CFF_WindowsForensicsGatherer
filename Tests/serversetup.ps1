78189cbeaa5c914b2aa0737d091ee08c581d7d85
jimtin <jameshin84@gmail.com>
2020-07-19 07:45:33 +1000

    Testing the test server

--- Tests/serversetup.ps1
+++ Tests/serversetup.ps1
@@ -1,4 +1,5 @@
 # Install Pester
 Install-Module Pester -Force -SkipPublisherCheck
-$version = Get-InstalledModule -Name Pester
-Write-Host $version
+
+# Remove old version of pester
+Remove-Item "C:\Program Files (x86)\WindowsPowerShell\Modules\Pester\3.4.0" -Recurse -Force
