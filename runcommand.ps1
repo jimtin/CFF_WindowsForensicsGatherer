e5ecb3874655c377b401aedfa329e6c4d726fd27
jimtin <jameshin84@gmail.com>
2020-07-20 05:59:26 +1000

    Constructing credentials

--- runcommand.ps1
+++ runcommand.ps1
@@ -2,11 +2,14 @@
 $target = $env:TARGET
 $playbook = $env:PLAYBOOK
 $username = $env:USER
-$securestring = $env:UPASS
+$securestring = ConvertTo-SecureString -String $env:UPASS -AsPlainText -Force
 
-# Construct the credentails
+# Write information to the commandline for reference
 Write-Host $username
 Write-Host $securestring
+
+# Construct the Credential object
+[pscredential]$creds = New-Object System.Management.Automation.PSCredential($username, $securestring) 
 
 if($playbook -eq "CITests"){
     # Inform user of actions being taken
