076830162a16fe32606b0720ca832d7e85ad7689
jimtin <jameshin84@gmail.com>
2020-07-21 05:18:07 +1000

    Starting Invoke-HostHunterCommand integration

--- Tests/serversetup.ps1
+++ Tests/serversetup.ps1
@@ -3,3 +3,6 @@ Install-Module Pester -Force -SkipPublisherCheck
 
 # Remove old version of pester
 Remove-Item "C:\Program Files (x86)\WindowsPowerShell\Modules\Pester\3.4.0" -Recurse -Force
+
+# Set the TrustedHosts Registry key to *. This is not great security practice, but for the purposes of testing is fine
+Set-Item WSMan:\localhost\Client\TrustedHosts * -Force
