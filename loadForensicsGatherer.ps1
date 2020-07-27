48bfb6f0cd3e3e2cf3b1072fbf29dcf3d62bf37b
jimtin <jameshin84@gmail.com>
2020-07-28 05:13:31 +1000

    Updated loading modules

--- loadForensicsGatherer.ps1
+++ loadForensicsGatherer.ps1
@@ -1,5 +1,5 @@
 # Load powershell modules
-$modules = Get-Content C:\WindowsForensicGatherer\CFF_WindowsForensicsGatherer-master\manifest.txt
+$modules = Get-Content C:\WindowsForensicsGatherer\CFF_WindowsForensicsGatherer-master\manifest.txt
 
 foreach ($cmdlet in $modules){
     Import-Module -Name $cmdlet -Force
