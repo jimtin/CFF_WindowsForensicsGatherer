8a64fd3f9fb7559e72ec360a2980b66458782669
jimtin <jameshin84@gmail.com>
2020-07-28 04:03:48 +1000

    Updated tests file

--- Tests/runCItests.ps1
+++ Tests/runCItests.ps1
@@ -1,14 +1,10 @@
 # Get the IP of this endpoint
-$netipaddress = Get-NetIPAddress | select AddressFamily, InterfaceAlias, IPaddress
-Write-Host $netipaddress
-$netipaddress = Get-NetIPAddress | Where-Object {$_.InterfaceAlias -eq "vEthernet (nat)"}
-$netipaddress = $netipaddress | Where-Object {$_.AddressFamily -eq "IPv4"}
+#$netipaddress = Get-NetIPAddress | select AddressFamily, InterfaceAlias, IPaddress
+#$netipaddress = Get-NetIPAddress | Where-Object {$_.InterfaceAlias -eq "vEthernet (nat)"}
+#$netipaddress = $netipaddress | Where-Object {$_.AddressFamily -eq "IPv4"}
 # Turn it into a string
-$netipaddress = $netipaddress.ToString()
+# $netipaddress = $netipaddress.ToString()
 $netipaddress = ($env:COMPUTERNAME).ToString()
-
-# Run the local host tests
-Invoke-Pester C:\Users\circleci\project\Tests\localHost.Tests.ps1
 
 # Pass the computername into the container. It does seem a little recursive in nature but is important
 $pass = ($env:COMPUTERNAME).ToString()
