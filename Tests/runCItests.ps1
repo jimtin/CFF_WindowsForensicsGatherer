01afa8eeaeea1cfda391fd217bad13c072194c07
jimtin <jameshin84@gmail.com>
2020-07-28 04:26:28 +1000

    Error in the runcommand

--- Tests/runCItests.ps1
+++ Tests/runCItests.ps1
@@ -1,10 +1,6 @@
 $username = $args[0]
 $password = $args[1]
-
-Write-Host $username
-
-# Get the IP of this endpoint
-$netipaddress = ($env:COMPUTERNAME).ToString()
+$target = $args[3]
 
 # Pass the computername into the container. It does seem a little recursive in nature but is important
-docker run -e Target=$netipaddress -e Playbook="CITests" -e USER=$username -e UPASS=$password -t cff_windowsforensicsgatherer
+docker run -e Target=$target -e Playbook="CITests" -e USER=$username -e UPASS=$password -t cff_windowsforensicsgatherer
